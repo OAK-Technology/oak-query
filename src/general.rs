02dc935f7c67cd905c672bc43dbb9d19898c854c
Ayaz <ayazcw@gmail.com>
2023-11-25 00:33:47 +0400

    enum ColumnType removed

--- src/general.rs
+++ src/general.rs
@@ -1,15 +1,6 @@
-use chrono::{NaiveDateTime, NaiveDate};
-use serde_json::Value;
 use sqlx::{Postgres, QueryBuilder};
 
 pub enum BaseQuery<'a> {
     Sql(&'a str),
     QueryBuilder(QueryBuilder<'a, Postgres>),
-}
-
-#[derive(Clone)]
-pub enum ColumnType {
-    OptPrimitive(Option<Value>),
-    OptDateTime(Option<NaiveDateTime>),
-    OptDate(Option<NaiveDate>),
 }
